5b948ab7e3a3f99775266a389fc411a68f8e3e44
bgocap <diego.capcha98@hotmail.com>
2023-09-07 18:51:23 +0300

    Exercise 4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -4,10 +4,26 @@ const dummy = (blogs) => {
     :1
   }
 const totalLikes = (blogs) =>{
-    return blogs.reduce((total,blog)=>total+=blog.likes,0)
+    const result =  blogs.reduce((total,blog)=>total+=blog.likes,0)
+    return result
+}
+
+const favoriteBlog = (blogs)=>{
+    if(blogs.length){
+        const highestNumOfLikes = blogs.reduce((currentMax,blog)=>{
+            return blog.likes>currentMax?blog.likes:currentMax;
+        },blogs[0].likes)
+        const favoriteBlog = blogs.find(blog=>blog.likes===highestNumOfLikes)
+        return ({
+            title : favoriteBlog.title,
+            author  : favoriteBlog.author,
+            likes : favoriteBlog.likes
+        })
+    }else{return{message:"no blog to show"}}
 }
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
